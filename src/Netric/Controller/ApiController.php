a12f36ce97d3120e02b920fd451b643b5032703f
Sky Stebnicki <sky.stebnicki@aereus.com>
2017-03-22 06:25:04 -0700

    Added a sync all endpoint

--- src/Netric/Controller/ApiController.php
+++ src/Netric/Controller/ApiController.php
@@ -23,7 +23,7 @@ class ApiController extends AbstractActionController
     }
     
     /**
-     * Synchronize one object by grabbing from remote store and saving locally
+     * Synchronize one entity by grabbing from remote store and saving locally
      * 
      * Netric can call http://<siteurl>/netric/api/sync-object?obj_type=<obj_type>&oid=<object_id>
      * to push object updates in real time.
@@ -51,6 +51,45 @@ class ApiController extends AbstractActionController
         return $response;
     }
 
+    /**
+     * Synchronize all entities by grabbing from remote store and saving locally
+     * 
+     * Netric can call http://<siteurl>/netric/api/sync-object?obj_type=<obj_type>&oid=<object_id>
+     * to push object updates in real time.
+     */
+    public function syncAllEntitiesAction()
+    {
+        // Get datamappers
+        $dmLocal = $this->getServiceLocator()->get("DataMapper");
+        $dmApi = $this->getServiceLocator()->get('DataMapperApi');
+
+        // Get options object type
+        $otype = $this->getRequest()->getQuery("obj_type");
+        
+        $types = [];
+        if ($otype) {
+            $types = [$otype];
+        } else {
+            $types = [
+                "cms_site",
+                "cms_snippet",
+                "cms_page",
+                "cms_page_template",
+                "content_feed",
+                "content_feed_post",
+            ];
+        }
+
+
+        foreach ($types as $typeToSync){
+            $dataMapperApi->syncCollection($typeToSync, $dataMapperLocal, true);
+        }
+        
+        // Return the id of the saved object, or 0 on failure
+        $response = $this->getResponse();
+        $response->setContent("done"); 
+        return $response;
+    }
 
     /**
      * Enter edit mode by setting a cookie
