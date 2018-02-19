3adef0e64f10e18392b041f09821dddf1aa5d704
Sky Stebnicki <sky@stebnicki.net>
2018-02-19 05:42:37 -0800

    Renaming readme so we can change the case

--- src/NetricZend/Controller/SearchController.php
+++ src/NetricZend/Controller/SearchController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Controller that handles basic blog functionality
  */
@@ -11,48 +12,43 @@ use Zend\View\Model\ViewModel;
 
 class SearchController extends AbstractActionController
 {
-    /**
-     * Show article index
-     * 
-     * @return \Zend\View\Model\ViewModel
-     */
-    public function indexAction()
+	/**
+	 * Show article index
+	 * 
+	 * @return \Zend\View\Model\ViewModel
+	 */
+	public function indexAction()
 	{
 		$config = $this->getServiceLocator()->get('Config');
 		$searcher = $this->getServiceLocator()->get('Searcher');
 
-		$q = $this->params()->fromQuery("q");
+		$queryString = $this->params()->fromQuery("q");
 
 		// Set object types
-		if (isset($config['netric']['search']['entities']))
-		{
-			foreach ($config['netric']['search']['entities'] as $objDef)
-			{
+		if (isset($config['netric']['search']['entities'])) {
+			foreach ($config['netric']['search']['entities'] as $objDef) {
 				$objType = $objDef['obj_type'];
 				$docType = $searcher->addType($objType);
 				$docType->urlBase = $objDef['url_base'];
-				
+
 				if (isset($objDef['label']))
 					$docType->typeLabel = $objDef['label'];
 
-				if (isset($objDef['conditions']))
-				{
-					foreach ($objDef['conditions'] as $fldName=>$fldVal)
+				if (isset($objDef['conditions'])) {
+					foreach ($objDef['conditions'] as $fldName => $fldVal)
 						$docType->addFilter("and", $fldName, "is_equal", $fldVal);
 				}
-				if (isset($objDef['fields']))
-				{
+				if (isset($objDef['fields'])) {
 					foreach ($objDef['fields'] as $fldName)
 						$docType->addSearchField($fldName);
 				}
 			}
 		}
 
-		$view = new ViewModel(array("q"=>$q));
+		$view = new ViewModel(array("q" => $queryString));
 
-		if ($q)
-		{
-			$res = $searcher->query($q);
+		if ($queryString) {
+			$res = $searcher->query($queryString);
 			if (count($res))
 				$view->results = $res;
 		}
